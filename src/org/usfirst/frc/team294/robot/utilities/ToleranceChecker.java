a0b11566da0c46f2cacba8d253c30814d08edc0b
dsawdai <dsawdai@ieee.org>
2016-02-19 20:43:12 -0800

    Started ToleranceChecker class

---
+++ src/org/usfirst/frc/team294/robot/utilities/ToleranceChecker.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team294.robot.utilities;
+
+/**
+ * Class used to check if a measured value is within a given
+ * tolerance for a given number of measurements.
+ * 
+ * @author Don Sawdai
+ *
+ */
+public class ToleranceChecker {
+
+}

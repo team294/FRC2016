69ddf99d53913c4d9e86d131f2b07a6444b559d1
Pavle Medvidovic <pakimed@gmail.com>
2016-03-02 19:29:08 -0800

    Committing changes so I can fix changes. GitHub is confusing

--- src/org/usfirst/frc/team294/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team294/robot/subsystems/Intake.java
@@ -12,34 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * Intake subsystem
  */
 public class Intake extends Subsystem {
-<<<<<<< HEAD
-	private final CANTalon intakeMotor = new CANTalon(RobotMap.intakeMotor);
-	private final DoubleSolenoid intakeSolenoid = new DoubleSolenoid(RobotMap.intakeSolenoidFwd, RobotMap.intakeSolenoidRev);
 
-
-	public Intake() {
-		// Call the Subsystem constructor
-		super();
-
-		// Set up subsystem components
-
-		// Add the subsystem to the LiveWindow
-		LiveWindow.addActuator("Intake", "Intake Motor", intakeMotor);
-		LiveWindow.addActuator("Intake", "Intake Solenoid", intakeSolenoid);
-	}
-
-
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
-
-	/**
-	 * Set speed of the intake rollers
-	 * @param speed Roller motor power, -1 (expel ball) to +1 (load ball).  0 = stopped
-	 */
-	public void setSpeed(double speed) {
-		intakeMotor.set(-speed);
-	}
-=======
     private final CANTalon intakeMotor = new CANTalon(RobotMap.intakeMotor);
     private final DoubleSolenoid intakeSolenoid = new DoubleSolenoid(RobotMap.intakeSolenoidFwd, RobotMap.intakeSolenoidRev);
    
@@ -59,82 +32,6 @@ public class Intake extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
-    /**
-     * Set speed of the intake rollers
-     * @param speed Roller motor power, -1 (expel ball) to +1 (load ball).  0 = stopped
-     */
-    public void setSpeed(double speed) {
-    	intakeMotor.set(-speed);
-    }
-    
-    /**
-     * Get speed of the intake rollers
-     * @return Roller motor power, -1 (expel ball) to +1 (load ball).  0 = stopped
-     */
-    public double getSpeed() {
-    	return -intakeMotor.get();
-    }
-    
-    /**
-     * Raise intake arm, if the shooter arm is not in the way.  If shooter arm is in
-     * the way, then this is ignored.
-     */
-    public void raiseIntake() {
-    	if (!shooterArmConflicts()) {
-    		intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
-    	}
-    }
-    
-    /**
-     * Lower intake arm
-     */
-    public void lowerIntake() {
-    	intakeSolenoid.set(DoubleSolenoid.Value.kForward);
-    }
-
-    /**
-     * Turn off piston solenoid
-     */
-    public void stopPiston() {
-    	intakeSolenoid.set(DoubleSolenoid.Value.kOff);
-    }
-    
-    /**
-     * Returns current solenoid setting for the intake arm.  <p>
-     * <b>NOTE</b>: The intake could currently be <i>moving</i> to this position
-     * and has not reached this position yet.
-     * @return true = intake is up, false = intake is down
-     */
-    public boolean intakeIsUp() {
-    	switch (intakeSolenoid.get()) {
-    	case kForward:
-    		SmartDashboard.putString("Intake position", "Forward");
-    		break;
-    	case kReverse:
-    		SmartDashboard.putString("Intake position", "Reverse");
-    		break;
-    	case kOff:
-    		SmartDashboard.putString("Intake position", "Off");
-    		break;
-    	}
-    	SmartDashboard.putBoolean("IntakeIsUp", intakeSolenoid.get()==DoubleSolenoid.Value.kReverse);
-    	return intakeSolenoid.get()==DoubleSolenoid.Value.kReverse;
-    }
-    
-    /**
-     * Checks to make sure that the shooter arm is not going to crash into the intake when raising the intake
-     * @return true when the shooter conflicts with the intake
-     * @return false when the shooter does not conflict with the intake
-     */
-    public boolean shooterArmConflicts() {
-    	double angleOfShooterArm = Robot.shooterArm.getAngle();
-    	if (angleOfShooterArm >= RobotMap.lowerBoundAngleToAvoid && angleOfShooterArm <= RobotMap.upperBoundAngleToAvoid){
-    		return true; 
-    	}
-    	return false; 
-    }
->>>>>>> refs/remotes/origin/master
 
 	/**
 	 * Get speed of the intake rollers

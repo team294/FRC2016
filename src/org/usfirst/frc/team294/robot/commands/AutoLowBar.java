3fdd73f878e947ea4eac33d8109a6f8b9666ef67
Emerson <team@laptop04>
2016-03-21 19:16:50 -0700

    Removed comments from the Auto classes.

--- src/org/usfirst/frc/team294/robot/commands/AutoLowBar.java
+++ src/org/usfirst/frc/team294/robot/commands/AutoLowBar.java
@@ -18,22 +18,5 @@ public class AutoLowBar extends CommandGroup {
     	addSequential(new DriveAngle(0.55, 45, false));				// 45 degrees from original orientation
     	addSequential(new DriveStraightDistance(0.6, 2.0*12.0, DriveStraightDistance.Units.inches));    	// "Slow" speed, 2 feet
     	addSequential(new DriveAngle(0.55, 0, false));				// Straighten up    	
-    	
-    	// Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
     }
 }

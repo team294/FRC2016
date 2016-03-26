aca37cce88aaa7ea536dd1307db234e2a6ef3ac2
Emerson <team@laptop04>
2016-03-25 19:17:56 -0700

    Low bar auto now goes to intake position.  Made port auto slower.

--- src/org/usfirst/frc/team294/robot/commands/AutoLowBar.java
+++ src/org/usfirst/frc/team294/robot/commands/AutoLowBar.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team294.robot.commands;
 
+import org.usfirst.frc.team294.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -13,7 +15,7 @@ public class AutoLowBar extends CommandGroup {
     	addSequential(new WaitSeconds(0.1));
     	addSequential(new ShiftDown());
     	addSequential(new IntakeLowerIfRaised());
-    	addSequential(new ShooterArmMoveToSetLocation(0));
+    	addSequential(new ShooterArmMoveToSetLocation(RobotMap.shooterArmBallLoadAngle));
     	addSequential(new DriveStraightDistance(0.6, 15.0*12.0, DriveStraightDistance.Units.inches));    	// "Slow" speed, 15 feet
     	addSequential(new DriveAngle(0.55, 45, false));				// 45 degrees from original orientation
     	addSequential(new DriveStraightDistance(0.6, 2.0*12.0, DriveStraightDistance.Units.inches));    	// "Slow" speed, 2 feet

4482ffdaf9aa29716d95478e32197480a14a08e8
Peter Johnson <johnson.peter@gmail.com>
2016-03-29 19:45:02 -0700

    RCSwitch: Use Jaguar instead of rolling our own PWM.

--- src/org/usfirst/frc/team294/robot/utilities/RCSwitch.java
+++ src/org/usfirst/frc/team294/robot/utilities/RCSwitch.java
@@ -1,24 +1,15 @@
 package org.usfirst.frc.team294.robot.utilities;
 
-import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Jaguar;
 
 public class RCSwitch {
-    private class RCSwitchPWM extends PWM {
-        public RCSwitchPWM(int channel) {
-            super(channel);
-            setBounds(2.1, 1.55, 1.5, 1.45, 0.9);
-            setPeriodMultiplier(PeriodMultiplier.k1X);
-            setRaw(0);
-            setZeroLatch();
-        }
-    }
-    RCSwitchPWM pwm;
+    Jaguar pwm;
     
     public RCSwitch(int channel) {
-        pwm = new RCSwitchPWM(channel);
+        pwm = new Jaguar(channel);
     }
     
     public void set(boolean on) {
-        pwm.setRaw(on ? 255 : 0);
+        pwm.set(on ? 1 : 0);
     }
 }
